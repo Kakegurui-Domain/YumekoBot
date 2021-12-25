f600512e5d7bde62d13ac25bf83041fe74ffb9df
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2021-12-25 01:17:31 -0800

    Update mhelp.py

--- Yumeko/modules/mhelp.py
+++ Yumeko/modules/mhelp.py
@@ -4,26 +4,26 @@ import os
 from telethon import events, Button
 from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
-from lunaBot.events import register as MEMEK
-from lunaBot import telethn as tbot
+from YumekoBot.events import register as MEMEK
+from YumekoBot import telethn as tbot
 
 PHOTO = "https://telegra.ph/file/7c3c26e0ed938aec91209.jpg"
 
 @MEMEK(pattern=("/mhelp"))
 async def awake(event):
   tai = event.sender.first_name
-  LUNA = "** ──「 Basic Guide 」── ** \n\n"
-  LUNA += "• /play **(song title) — To Play the song you requested via YouTube** \n"
-  LUNA += "• /search ** (song/video title) – To search for links on YouTube with details** \n"
-  LUNA += "• /playlist - **show the list song in queue** \n"
-  LUNA += "•/lyric - ** (song name) lyrics scrapper** \n\n"
-  LUNA += "** ──「 Admin CMD 」── ** \n\n"
-  LUNA += "• /pause - **To Pause Song playback** \n"
-  LUNA += "• /resume - **To resume playback of the paused Song** \n"
-  LUNA += "• /skip - **To Skip playback of the song to the next Song** \n"
-  LUNA += "• /end - **To Stop Song playback** \n"
-  LUNA += "• /control - **open the player settings panel** \n"
-  LUNA += "• /reload - **To Refresh admin list** \n"
+  YUMEKO = "** ──「 Basic Guide 」── ** \n\n"
+  YUMEKO += "• /play **(song title) — To Play the song you requested via YouTube** \n"
+  YUMEKO += "• /search ** (song/video title) – To search for links on YouTube with details** \n"
+  YUMEKO += "• /playlist - **show the list song in queue** \n"
+  YUMEKO += "•/lyric - ** (song name) lyrics scrapper** \n\n"
+  YUMEKO += "** ──「 Admin CMD 」── ** \n\n"
+  YUMEKO += "• /pause - **To Pause Song playback** \n"
+  YUMEKO += "• /resume - **To resume playback of the paused Song** \n"
+  YUMEKO += "• /skip - **To Skip playback of the song to the next Song** \n"
+  YUMEKO += "• /end - **To Stop Song playback** \n"
+  YUMEKO += "• /control - **open the player settings panel** \n"
+  YUMEKO += "• /reload - **To Refresh admin list** \n"
 
   BUTTON = [[Button.url("☎️ Support", "https://t.me/lunaXresso"), Button.url("📡 Updates", "https://t.me/ZeinzoProject")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=LUNA,  buttons=BUTTON)
