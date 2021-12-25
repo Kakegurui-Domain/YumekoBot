587f98f0722af638b514b2a5af38432659d2fc49
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2021-12-25 00:35:56 -0800

    Update __speachtotext.py

--- Yumeko/modules/__speachtotext.py
+++ Yumeko/modules/__speachtotext.py
@@ -1,4 +1,4 @@
-from lunaBot import telethn as tbot
+from YumekoBot import telethn as tbot
 import os
 import urllib.request
 from datetime import datetime
@@ -11,8 +11,8 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from lunaBot import *
-from lunaBot.events import register
+from YumekoBot import *
+from YumekoBot.events import register
 
 
 async def is_register_admin(chat, user):
