e439fa741f658082b264d0dc7dd5709f34c017f5
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2021-12-25 00:28:41 -0800

    Update __core.py

--- Yumeko/modules/__core.py
+++ Yumeko/modules/__core.py
@@ -1,15 +1,15 @@
-from lunaBot import telethn as tbot
-from lunaBot.events import register
+from YumekoBot import telethn as tbot
+from YumekoBot.events import register
 import os
 import asyncio
 import os
 import time
 from datetime import datetime
-from lunaBot import OWNER_ID, DEV_USERS
-from lunaBot import TEMP_DOWNLOAD_DIRECTORY as path
-from lunaBot import TEMP_DOWNLOAD_DIRECTORY
+from YumekoBot import OWNER_ID, DEV_USERS
+from YumekoBot import TEMP_DOWNLOAD_DIRECTORY as path
+from YumekoBot import TEMP_DOWNLOAD_DIRECTORY
 from datetime import datetime
-water = './lunaBot/resources/yone.jpg'
+water = './YumekoBot/resources/yone.jpg'
 client = tbot
 
 @register(pattern=r"^/send ?(.*)")
@@ -21,7 +21,7 @@ async def Prof(event):
     thumb = water
     message_id = event.message.id
     input_str = event.pattern_match.group(1)
-    the_plugin_file = "./lunaBot/modules/{}.py".format(input_str)
+    the_plugin_file = "./YumekoBot/modules/{}.py".format(input_str)
     if os.path.exists(the_plugin_file):
      message_id = event.message.id
      await event.client.send_file(
