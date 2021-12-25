9d2dcddad26ee2be4211e786cff96f2cdd44c34d
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2021-12-25 02:19:45 -0800

    Update math.py

--- Yumeko/modules/math.py
+++ Yumeko/modules/math.py
@@ -1,8 +1,8 @@
 import math
 
 import pynewtonmath as newton
-from YumekoBot import dispatcher
-from YumekoBot.modules.disable import DisableAbleCommandHandler
+from Yumeko import dispatcher
+from Yumeko.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
