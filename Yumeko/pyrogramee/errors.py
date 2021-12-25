76747dd14894fb9b1ed5aba729877c5cc61dc2a0
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2021-12-25 13:26:11 +0530

    Yumeko

--- Yumeko/pyrogramee/errors.py
+++ Yumeko/pyrogramee/errors.py
@@ -1,7 +1,7 @@
 import sys
 import traceback
 from functools import wraps
-from lunaBot import pbot, SUPPORT_CHAT
+from Yumeko import pbot, SUPPORT_CHAT
 
 def split_limits(text):
     if len(text) < 2048:
