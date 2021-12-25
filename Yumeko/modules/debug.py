ec3a6ffd4011fc1e1d9173ed8ffb6ccfdf23aea6
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2021-12-25 01:46:57 -0800

    Update debug.py

--- Yumeko/modules/debug.py
+++ Yumeko/modules/debug.py
@@ -5,8 +5,8 @@ from telethon import events
 from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
-from lunaBot import telethn, dispatcher
-from lunaBot.modules.helper_funcs.chat_status import dev_plus
+from YumekoBot import telethn, dispatcher
+from YumekoBot.modules.helper_funcs.chat_status import dev_plus
 
 DEBUG_MODE = False
 
