aa20b6013998ecf0cf5ba2c7208722c888f1a0cd
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2021-12-25 17:04:21 +0530

    Update zombies.py

--- Yumeko/modules/zombies.py
+++ Yumeko/modules/zombies.py
@@ -114,7 +114,7 @@ async def zombies(event):
     
     
 from telethon.tl.types import UserStatusLastMonth, UserStatusLastWeek, ChatBannedRights
-from lunaBot.events import register
+from Yumeko.events import register
 from telethon import *
 from telethon.tl.functions.channels import (EditBannedRequest)
                                             
