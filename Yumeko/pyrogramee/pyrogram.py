ec6da9e25cfa9992be69e2620d457fcad0a39c60
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2021-12-25 12:11:58 +0530

    Yumeko Commit

---
+++ Yumeko/pyrogramee/pyrogram.py
@@ -0,0 +1,22 @@
+import logging
+
+from pyrogram import Client
+
+# from pyromod import listen
+from lunaBot.conf import get_int_key, get_str_key
+
+TOKEN = get_str_key("TOKEN", required=True)
+APP_ID = get_int_key("API_ID", required=True)
+APP_HASH = get_str_key("API_HASH", required=True)
+session_name = TOKEN.split(":")[0]
+pbot = Client(
+    session_name,
+    api_id=APP_ID,
+    api_hash=APP_HASH,
+    bot_token=TOKEN,
+)
+
+# disable logging for pyrogram [not for ERROR logging]
+logging.getLogger("pyrogram").setLevel(level=logging.ERROR)
+
+pbot.start()
