ba3d47ae4dce534524a6f15740eb4dc37bb91854
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2021-12-25 14:54:07 +0530

    Update notes_sql.py

--- Yumeko/modules/sql/notes_sql.py
+++ Yumeko/modules/sql/notes_sql.py
@@ -1,8 +1,8 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from lunaBot.modules.helper_funcs.msg_types import Types
-from lunaBot.modules.sql import BASE, SESSION
+from Yumeko.modules.helper_funcs.msg_types import Types
+from Yumeko.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, BigInteger, String, UnicodeText, distinct, func
 
 
