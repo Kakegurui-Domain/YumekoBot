a7faba5fe8236730f0f33bd5842b0980863753ba
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2021-12-25 15:59:36 +0530

    Update dev.py

--- Yumeko/modules/dev.py
+++ Yumeko/modules/dev.py
@@ -5,10 +5,10 @@ import sys
 from contextlib import suppress
 from time import sleep
 
-import YumekoBot
+import Yumeko
 
-from YumekoBot import dispatcher
-from YumekoBot.modules.helper_funcs.chat_status import dev_plus
+from Yumeko import dispatcher
+from Yumeko.modules.helper_funcs.chat_status import dev_plus
 from telegram import TelegramError, Update
 from telegram.error import Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, run_async
