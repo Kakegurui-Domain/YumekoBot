b472e8c872ced086ffc4332450cb8807e7d33813
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2021-12-25 00:29:07 -0800

    Update __country.py

--- Yumeko/modules/__country.py
+++ Yumeko/modules/__country.py
@@ -5,8 +5,8 @@ from telethon.utils import get_input_location
 import flag
 import html, os
 from countryinfo import CountryInfo
-from lunaBot import telethn as borg
-from lunaBot.events import register
+from YumekoBot import telethn as borg
+from YumekoBot.events import register
 
 
 @register(pattern="^/country (.*)")
