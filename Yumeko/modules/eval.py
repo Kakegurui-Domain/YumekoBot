5703f5a80a4d532e9990664c95096580f254af88
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2021-12-25 02:27:32 -0800

    Update eval.py

--- Yumeko/modules/eval.py
+++ Yumeko/modules/eval.py
@@ -6,8 +6,8 @@ import textwrap
 import traceback
 from contextlib import redirect_stdout
 
-from lunaBot import LOGGER, dispatcher
-from lunaBot.modules.helper_funcs.chat_status import dev_plus
+from Yumeko import LOGGER, dispatcher
+from Yumeko.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
