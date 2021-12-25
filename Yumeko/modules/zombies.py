a7025c18d46666ef2871fab396d1e147edeb54eb
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2021-12-25 00:39:06 -0800

    Update zombies.py

--- Yumeko/modules/zombies.py
+++ Yumeko/modules/zombies.py
@@ -6,7 +6,7 @@ from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins
 
-from lunaBot import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
+from YumekoBot import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
 
 # =================== CONSTANT ===================
 
