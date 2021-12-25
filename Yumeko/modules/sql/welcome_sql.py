f4e91e3fb4efe73466995d2f407972d0a760f438
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2021-12-25 14:58:15 +0530

    Update welcome_sql.py

--- Yumeko/modules/sql/welcome_sql.py
+++ Yumeko/modules/sql/welcome_sql.py
@@ -2,8 +2,8 @@ import random
 import threading
 from typing import Union
 
-from lunaBot.modules.helper_funcs.msg_types import Types
-from lunaBot.modules.sql import BASE, SESSION
+from Yumeko.modules.helper_funcs.msg_types import Types
+from Yumeko.modules.sql import BASE, SESSION
 from sqlalchemy import BigInteger, Boolean, Column, BigInteger, String, UnicodeText
 
 DEFAULT_WELCOME = "Hey {first}, how are you?"
