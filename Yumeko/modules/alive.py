71b77c93a85c295d725a7324735d9c2405e9a306
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2021-12-25 15:42:52 +0530

    Update alive.py

--- Yumeko/modules/alive.py
+++ Yumeko/modules/alive.py
@@ -4,26 +4,26 @@ import os
 from telethon import events, Button
 from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
-from YumekoBot.events import register as MEMEK
-from YumekoBot import telethn as tbot
+from Yumeko.events import register as MEMEK
+from Yumeko import telethn as tbot
 
 PHOTO = "https://telegra.ph/file/7c3c26e0ed938aec91209.jpg"
 
 @MEMEK(pattern=("/alive"))
 async def awake(event):
   tai = event.sender.first_name
-  YUMEKO = "**Holla I'm Luna!** \n\n"
+  YUMEKO = "**Holla I'm Yumeko!** \n\n"
   YUMEKO += "🔴 **I'm Working Properly** \n\n"
-  YUMEKO += "🔴 **My Master : [zeinzo](https://t.me/tdrki_1)** \n\n"
+  YUMEKO += "🔴 **My Owners : [Tarun • [ᴀc͜͡ɢᴄ]](https://t.me/TheBlacklinen)[亗 ʀʏᴜ](https://t.me/Ryu_God)** \n\n"
   YUMEKO += f"🔴 **Telethon Version : {tlhver}** \n\n"
   YUMEKO += f"🔴 **Pyrogram Version : {pyrover}** \n\n"
   YUMEKO += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/lunatapibot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/lunaXresso")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=LUNA,  buttons=BUTTON)
+  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/YumekoProBot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/lunaXresso")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=YUMEKO,  buttons=BUTTON)
 
 @MEMEK(pattern=("/reload"))
 async def reload(event):
   tai = event.sender.first_name
-  LUNA = "✅ **bot restarted successfully**\n\n• Admin list has been **updated**"
-  BUTTON = [[Button.url("📡 ᴜᴘᴅᴀᴛᴇs", "https://t.me/zeinproject")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=LUNA,  buttons=BUTTON)
+  YUMEKO = "✅ **bot restarted successfully**\n\n• Admin list has been **updated**"
+  BUTTON = [[Button.url("📡 ᴜᴘᴅᴀᴛᴇs", "https://t.me/")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=YUMEKO,  buttons=BUTTON)
