dc4a945f57a0bcab2de65b3dc35de73ee2187531
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2021-12-25 01:40:49 -0800

    Update connection.py

--- Yumeko/modules/connection.py
+++ Yumeko/modules/connection.py
@@ -5,10 +5,10 @@ from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Upda
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CommandHandler, CallbackQueryHandler, run_async
 
-import lunaBot.modules.sql.connection_sql as sql
-from lunaBot import dispatcher, DRAGONS, DEV_USERS
-from lunaBot.modules.helper_funcs import chat_status
-from lunaBot.modules.helper_funcs.alternate import send_message, typing_action
+import YumekoBot.modules.sql.connection_sql as sql
+from YumekoBot import dispatcher, DRAGONS, DEV_USERS
+from YumekoBot.modules.helper_funcs import chat_status
+from YumekoBot.modules.helper_funcs.alternate import send_message, typing_action
 
 user_admin = chat_status.user_admin
 
