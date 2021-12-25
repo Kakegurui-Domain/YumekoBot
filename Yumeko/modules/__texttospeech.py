4eeb0ea7a2798d80b89a1a4017b8d9864c7d6f83
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2021-12-25 15:36:07 +0530

    Update __texttospeech.py

--- Yumeko/modules/__texttospeech.py
+++ Yumeko/modules/__texttospeech.py
@@ -1,4 +1,4 @@
-from lunaBot import telethn as tbot
+from Yumeko import telethn as tbot
 import os
 
 from gtts import gTTS
@@ -8,9 +8,9 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from lunaBot import *
+from Yumeko import *
 
-from lunaBot.events import register
+from Yumeko.events import register
 
 
 async def is_register_admin(chat, user):
