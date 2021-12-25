9371aacff93a5529b5a68bd47c2d889a2c135716
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2021-12-25 01:55:05 -0800

    Update wallpaper.py

--- Yumeko/modules/wallpaper.py
+++ Yumeko/modules/wallpaper.py
@@ -1,8 +1,8 @@
 from random import randint
 
 import requests as r
-from YumekoBot import SUPPORT_CHAT, WALL_API, dispatcher
-from YumekoBot.modules.disable import DisableAbleCommandHandler
+from Yumeko import SUPPORT_CHAT, WALL_API, dispatcher
+from Yumeko.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
