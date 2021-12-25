ef3fe95f4139924d95934493d905f40bfb6a1b90
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2021-12-25 01:05:39 -0800

    Update remote_cmds.py

--- Yumeko/modules/remote_cmds.py
+++ Yumeko/modules/remote_cmds.py
@@ -1,12 +1,12 @@
-from lunaBot import dispatcher
-from lunaBot.modules.helper_funcs.chat_status import (
+from YumekoBot import dispatcher
+from YumekoBot.modules.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from lunaBot.modules.helper_funcs.extraction import extract_user_and_text
-from lunaBot.modules.helper_funcs.filters import CustomFilters
+from YumekoBot.modules.helper_funcs.extraction import extract_user_and_text
+from YumekoBot.modules.helper_funcs.filters import CustomFilters
 from telegram import Update, ChatPermissions
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
