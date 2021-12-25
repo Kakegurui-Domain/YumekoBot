126de0cb5fd28333253269e44163be6545940f29
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2021-12-25 01:59:25 -0800

    Update speed_test.py

--- Yumeko/modules/speed_test.py
+++ Yumeko/modules/speed_test.py
@@ -1,7 +1,7 @@
 import speedtest
-from YumekoBot import DEV_USERS, dispatcher
-from YumekoBot.modules.disable import DisableAbleCommandHandler
-from YumekoBot.modules.helper_funcs.chat_status import dev_plus
+from Yumeko import DEV_USERS, dispatcher
+from Yumeko.modules.disable import DisableAbleCommandHandler
+from Yumeko.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
 
