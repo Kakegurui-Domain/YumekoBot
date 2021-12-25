01a6880f3db061c82be55ae443a3c8fe02853029
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2021-12-25 01:09:57 -0800

    Update muting.py

--- Yumeko/modules/muting.py
+++ Yumeko/modules/muting.py
@@ -1,20 +1,20 @@
 import html
 from typing import Optional
 
-from lunaBot import LOGGER, TIGERS, dispatcher
-from lunaBot.modules.helper_funcs.chat_status import (
+from YumekoBot import LOGGER, TIGERS, dispatcher
+from YumekoBot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     connection_status,
     is_user_admin,
     user_admin,
 )
-from lunaBot.modules.helper_funcs.extraction import (
+from YumekoBot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from lunaBot.modules.helper_funcs.string_handling import extract_time
-from lunaBot.modules.log_channel import loggable
+from YumekoBot.modules.helper_funcs.string_handling import extract_time
+from YumekoBot.modules.log_channel import loggable
 from telegram import Bot, Chat, ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
