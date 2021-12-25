fce778fb00165fb75e61c965f4cd7bc908f07c2a
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2021-12-25 01:06:11 -0800

    Update reactions.py

--- Yumeko/modules/reactions.py
+++ Yumeko/modules/reactions.py
@@ -1,7 +1,7 @@
 import random
 
-from lunaBot import dispatcher
-from lunaBot.modules.disable import DisableAbleCommandHandler
+from YumekoBot import dispatcher
+from YumekoBot.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
