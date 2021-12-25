b9e65d08ee01693808b5e1fe43889c010f5468fe
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2021-12-25 14:40:54 +0530

    Update msg_types.py

--- Yumeko/modules/helper_funcs/msg_types.py
+++ Yumeko/modules/helper_funcs/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from lunaBot.modules.helper_funcs.string_handling import button_markdown_parser
+from Yumeko.modules.helper_funcs.string_handling import button_markdown_parser
 from telegram import Message
 
 
