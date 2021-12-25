9b212da658dd8354751b187262e1441e58cc000d
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2021-12-25 00:32:33 -0800

    Update __heroku.py

--- Yumeko/modules/__heroku.py
+++ Yumeko/modules/__heroku.py
@@ -5,8 +5,8 @@ import os
 import heroku3
 import requests
 
-from lunaBot import telethn as borg, HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
-from lunaBot.events import register
+from YumekoBot import telethn as borg, HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
+from YumekoBot.events import register
 
 heroku_api = "https://api.heroku.com"
 Heroku = heroku3.from_key(HEROKU_API_KEY)
