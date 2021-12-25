3e67b8f4a7235a69226fda7c55eb6fd660a6084e
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2021-12-25 14:52:57 +0530

    Update locks_sql.py

--- Yumeko/modules/sql/locks_sql.py
+++ Yumeko/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from lunaBot.modules.sql import SESSION, BASE
+from Yumeko.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
