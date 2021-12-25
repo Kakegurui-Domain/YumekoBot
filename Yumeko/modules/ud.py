744b0d67f741bbb590d8b5e280403f5cb3a6f7e5
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2021-12-25 01:57:14 -0800

    Update ud.py

--- Yumeko/modules/ud.py
+++ Yumeko/modules/ud.py
@@ -1,6 +1,6 @@
 import requests
-from YumekoBot import dispatcher
-from YumekoBot.modules.disable import DisableAbleCommandHandler
+from Yumeko import dispatcher
+from Yumeko.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
