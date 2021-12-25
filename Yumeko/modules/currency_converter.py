3fe2bb6f932086c4fbd2fd7c5ef055bd00800d27
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2021-12-25 15:56:07 +0530

    Update currency_converter.py

--- Yumeko/modules/currency_converter.py
+++ Yumeko/modules/currency_converter.py
@@ -1,5 +1,5 @@
 import requests
-from YumekoBot import CASH_API_KEY, dispatcher
+from Yumeko import CASH_API_KEY, dispatcher
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
