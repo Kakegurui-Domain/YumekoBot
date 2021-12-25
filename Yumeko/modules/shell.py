a7f2c3ab3331e1e2004b17e827a451729d67dfca
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2021-12-25 01:03:12 -0800

    Update shell.py

--- Yumeko/modules/shell.py
+++ Yumeko/modules/shell.py
@@ -1,7 +1,7 @@
 import subprocess
 
-from lunaBot import LOGGER, dispatcher
-from lunaBot.modules.helper_funcs.chat_status import dev_plus
+from YumekoBot import LOGGER, dispatcher
+from YumekoBot.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async
