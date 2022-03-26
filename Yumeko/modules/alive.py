974c74cb4a3da76d71c7e08105507491092d704f
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2022-03-27 00:50:46 +0530

    Update alive.py

--- Yumeko/modules/alive.py
+++ Yumeko/modules/alive.py
@@ -7,23 +7,23 @@ from pyrogram import __version__ as pyrover
 from Yumeko.events import register as MEMEK
 from Yumeko import telethn as tbot
 
-PHOTO = "https://telegra.ph/file/e0040466048302df1a451.mp4"
+PHOTO = "@https://telegra.ph/file/ec320ac49dd361caacad6.mp4"
 
 @MEMEK(pattern=("/alive"))
 async def awake(event):
   tai = event.sender.first_name
-  YUMEKO = "**Holla I'm Yumeko!** \n\n"
+  YUMEKO = "**Holla I'm Rikka!** \n\n"
   YUMEKO += "×**I'm Working Properly** \n\n"
-  YUMEKO += "×**My Owners : [Tarun • [ᴀc͜͡ɢᴄ]](https://t.me/TheBlacklinen), [亗 ʀʏᴜ](https://t.me/Ryu_God)** \n\n"
+  YUMEKO += "×**My Owner : [Sneha Jha](https://t.me/Sneha_UwU_OwO)** \n\n"
   YUMEKO += f"×**Telethon Version : {tlhver}** \n\n"
   YUMEKO += f"×**Pyrogram Version : {pyrover}** \n\n"
   YUMEKO += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/YumekoProBot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/Rikka_Tyrant_bot?start=help"), Button.url("Support", "https://t.me/tyranteyeeee")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=YUMEKO,  buttons=BUTTON)
 
 @MEMEK(pattern=("/reload"))
 async def reload(event):
   tai = event.sender.first_name
   YUMEKO = "✅ **bot restarted successfully**\n\n• Admin list has been **updated**"
-  BUTTON = [[Button.url("📡 ᴜᴘᴅᴀᴛᴇs", "https://t.me/")]]
+  BUTTON = [[Button.url("📡 ᴜᴘᴅᴀᴛᴇs", "https://t.me/tyranteyeeee")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=YUMEKO,  buttons=BUTTON)
