cc2fa5b44bd06b14d44178220c12294480964fc2
Lynnxept <98268117+Lynnxept@users.noreply.github.com>
2022-03-28 12:54:47 +0530

    Update misc.py

--- Yumeko/modules/helper_funcs/misc.py
+++ Yumeko/modules/helper_funcs/misc.py
@@ -68,7 +68,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if calc in [1, 2]:
         pairs.append([modules[-1]])
     else:
-        pairs += [[EqInlineKeyboardButton("Home", callback_data="Yumeko_back")]]
+        pairs += [[EqInlineKeyboardButton("Home", callback_data="rikka_back")]]
     return pairs
 
 
